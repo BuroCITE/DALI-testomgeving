bd08fe7c02bdfdeeabd6c63f0381e1b03f107a89
Anthony <anthonyair03@gmail.com>
2023-01-25 16:00:33 +0100

    semantic tags toegevoegd voor Login. kleine css aanpassingen voor Login. css voor Home uitgecomment. (de css is op een manier geschreven dat deze in de weg zit van Login css)

--- dali-home/src/js/pages/inlog.js
+++ dali-home/src/js/pages/inlog.js
@@ -10,45 +10,52 @@ export class Login extends React.Component {
   render() {
     return (
     <>
-      <main>
-        {/* <img src="https://random.imagecdn.app/1700/850"/> */}
+
         <Background/>
-        <div class="inlog-shadow">
+        <aside class="inlog-shadow">
           <div class="inlog-sidebar">
-            <img src={`${process.env.PUBLIC_URL}/image/Dali-logo-white-bg.jpg`}/>
+            <header>
+              <figure class="header-image-container">
+                <img src={`${process.env.PUBLIC_URL}/image/Dali-logo-white-bg.jpg`}/>
+                <figcaption>Het logo van dali</figcaption>
+              </figure>
 
-            <div class="dali-slogan">
-              <p>de oplossing voor buitenruimtemanagement</p>
-              <br/>
-              <h1>DALI v5 demo</h1>
-            </div>
+              <article class="dali-slogan">
+                <p>de oplossing voor buitenruimtemanagement</p>
+                <br/>
+                <h1>DALI v5 demo</h1>
+                <details>
+                  <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions, award-winning fireworks and seasonal special events.</p>
+                </details>
+              </article>
+            </header>
 
-            <form>
-              <label for="userName">gebruikersnaam</label>
-              <div class="icon-input">
-              <span><i class="fa fa-users fa"></i></span>
-                <input name="userName" placeholder='Gebruikersnaam'></input>
-              </div>
-              
+            <main>
+              <form>
+                <label for="userName">gebruikersnaam</label>
+                <div class="icon-input">
+                  <span><i class="fa fa-users fa"></i></span>
+                  <input name="userName" placeholder='Gebruikersnaam'/>
+                </div>
 
-              <label for="password">wachtwoord</label>
-              <div class="icon-input">
-                <span><i class="fa fa-lock fa"></i></span>
-                <input name="password" type="password" placeholder='Wachtwoord'></input>
-              </div>
+                <label for="password">wachtwoord</label>
+                <div class="icon-input">
+                  <span><i class="fa fa-lock fa"></i></span>
+                  <input name="password" type="password" placeholder='Wachtwoord'/>
+                </div>
 
-              <button type='submit'>Aanmelden</button>
-              <br/>
-              <button>Wachtwoord vergeten?</button>
-              <button><i class="fa fa-envelope fa"></i> Mail naar DALI-Helpdesk</button>
-            </form>
+                <button type='submit'>Aanmelden</button>
+                <br/>
+                <button>Wachtwoord vergeten?</button>
+                <button><i class="fa fa-envelope fa"></i> Mail naar DALI-Helpdesk</button>
+              </form>
+            </main>
 
             <footer>
               <p>Versie: 5.0.0.0</p>
             </footer>
           </div>
-        </div>
-      </main>
+        </aside>
     </>
     );
   }
