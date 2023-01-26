b040c321ef072df91334ca4c2e31ea451e18cff1
Anthony <anthonyair03@gmail.com>
2023-01-26 10:35:35 +0100

    Form components added. input fields changed to Form components.

---
+++ dali-home/src/js/components/form.js
@@ -0,0 +1,125 @@
+import React from 'react';
+
+export class Form extends React.Component {
+  constructor(props){
+    super(props);
+    // TODO: state needs to be updated to something usefull
+    this.state = {projectName: 'Anthony'};
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  /**
+   * changes the projectName in the state to whatever was in the event targets value
+   * @param evt een object with all data regarding the events of this class 
+   */
+  handleChange(evt){
+    this.setState(() => {
+      return {projectName: evt.target.value}; 
+    });
+  }
+  
+  render() {
+    return (
+      <form>
+        {this.props.children}
+      </form>
+    );
+  }
+}
+
+/**
+ * *this doc block contains param's that are used in almost all functions under it. so this doc block will serve as a legenda for these param's
+ * @param props props is een React object waar alle data aan wordt meegegeven.
+ * @param props.gridLocation verwacht een className waar de grid positie in staat. werkt alleen als deze in een grid zit.
+ * TODO: make the css a scss by incorporating SASS.
+ * TODO: make default styling options where developer can choice from (just like test-form-v2).
+ * @param props.className verwacht een className met de gewenste styling van de input en label.
+ * ? should props.name also be used for the input id
+ * @param props.Name the name for the input and name used to connect the label.
+ * @param props.label the text shown inside the label
+ * @param props.type here you can add a type to your input 
+ * * the next 2 param where made with the intention to call "this" from this Form and not the class where this function is called
+ * @param props.handleChange if the input has to do a action when change occurs a function can be connected in this variable. function has to be in the Form class
+ * @param props.state expects {this.state}. so you can use state inside a different class.
+ * * the placeholder is nessesary for css styling and should always contain a space, but cannot have a value due to accesibility reasons.
+ */
+
+
+/**
+ * *all param's are located in the doc block above.
+ * this function exports a input field with label. the data that is expected is sent via props.
+ * @returns a input field with a label and icon inside a div
+ */
+export function Input(props) {
+  return (
+    <div className={`${props.gridLocation} ${props.className}`}>
+      <label htmlFor={props.name}>{props.label}</label>
+      <input placeholder=" " id={props.name} name={props.name} type={props.type} onChange={props.handleChange} defaultValue={props.state.projectName}/>
+    </div>
+  );
+}
+
+
+/**
+ * *all param's are located in the doc block above.
+ * this function exports a input field with label and a icon. the data that is expected is sent via props.
+ * @returns a input field with a label inside a div
+ */
+export function IconInput(props) {
+  return (
+    <div className={`${props.gridLocation} ${props.className}`}>
+      <label htmlFor={props.name}>{props.label}</label>
+      <span><i className={props.iconClass}></i></span>
+      <input placeholder=" " id={props.name} name={props.name} type={props.type} onChange={props.handleChange} defaultValue={props.state.projectName}/>
+    </div>
+  );
+}
+
+
+/**
+ * *some param's are located in the doc block above.
+ * this function exports a select dropdown with label.
+ * @param props.variables expects a array of objects with the values name and value.
+ * @returns a select field with a label inside a div
+ */
+export function Select(props) {
+  return (
+    <div className={`${props.gridLocation} ${props.className}`}>
+      <label htmlFor={props.name}>{props.label}</label>
+      <select id={props.name} name={props.name} onChange={props.handleChange}>
+        <option value="0" >choose a option.</option>
+        {props.variables.map(variable => (  
+          <option key={variable.name} value={variable.value} >{variable.name}</option>
+        ))};
+      </select>
+    </div>
+  );
+}
+
+/**
+ * *all param's are located in the doc block above.
+ * this function exports a text area. the data that is expected is sent via props.
+ * @returns a text area with a label
+ */
+export function Textarea(props) {
+  return (
+    <div className={`${props.gridLocation} ${props.className}`}>
+      <label htmlFor={props.name}>{props.label}</label>
+      <textarea id={props.name} name={props.name} onChange={props.handleChange}/>
+    </div>
+  );
+}
+
+/**
+ * *some param's are located in the doc block above.
+ * this function exports a text area. the data that is expected is sent via props.
+ * @param props.title here you can add a title to show in the upload
+ * @returns an upload field inside a div
+ */
+export function Upload(props) {
+  return (
+    <div className={props.gridLocation}>
+      <input title={props.title} type="file" id={props.idName} className={props.className} name={props.name} onChange={props.handleChange}/>
+    </div> 
+  );
+}
--- dali-home/src/js/pages/inlog.js
+++ dali-home/src/js/pages/inlog.js
@@ -1,6 +1,8 @@
 import '../../css/Inlog.css';
 import React from 'react';
 import { Background } from '../components/background';
+import {Form} from '../components/form';
+import {Input, IconInput, Select, Textarea, Upload} from '../components/form';
 
 export class Login extends React.Component {
   constructor(props){
@@ -24,31 +26,18 @@ export class Login extends React.Component {
                 <p>de oplossing voor buitenruimtemanagement</p>
                 <br/>
                 <h1>DALI v5 demo</h1>
-                <details>
-                  <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions, award-winning fireworks and seasonal special events.</p>
-                </details>
               </article>
             </header>
 
             <main>
-              <form>
-                <label for="userName">gebruikersnaam</label>
-                <div class="icon-input">
-                  <span><i class="fa fa-users fa"></i></span>
-                  <input name="userName" placeholder='Gebruikersnaam'/>
-                </div>
-
-                <label for="password">wachtwoord</label>
-                <div class="icon-input">
-                  <span><i class="fa fa-lock fa"></i></span>
-                  <input name="password" type="password" placeholder='Wachtwoord'/>
-                </div>
-
+              <Form>
+                <IconInput gridLocation="" type="" name="username" className="icon-input" label="gebruikersnaam" iconClass="fa fa-users fa" handleChange="" state=""/>
+                <IconInput gridLocation="" type="password" name="password" className="icon-input" label="wachtwoord" iconClass="fa fa-users fa" handleChange="" state=""/>
                 <button type='submit'>Aanmelden</button>
                 <br/>
                 <button>Wachtwoord vergeten?</button>
                 <button><i class="fa fa-envelope fa"></i> Mail naar DALI-Helpdesk</button>
-              </form>
+              </Form>
             </main>
 
             <footer>
