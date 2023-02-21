20ce415c3c3d2d15483467ae9c2e7f4c3a2fe9a9
Anthony <anthonyair03@gmail.com>
2023-02-21 10:45:53 +0100

    split dropdown css from home css to make dropdown fully reusable, make new nav inside header.js for right side navigation, small changes on the default css of dropdown, header, popup, card components, added docblocksto different components namely for:     dropdown,     header,     popup; replaced popup id's with ref's inside the componentDidMount() so that popup could be fully reusable even adjecent to a other popup. this was previously not possible, transformed Home class to a functional component;

--- dali-home/src/js/components/dropdown.js
+++ dali-home/src/js/components/dropdown.js
@@ -1,35 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import { useAuth } from '../auth/useAuth';
-
+import React, { useEffect, useState, useRef } from 'react';
+/**
+ * 
+ * @param {string} buttonClass a class that you can assign to the dropdown button. expects a string
+ * @param {element} buttonIcon the icon you want to use in the dropdown button. expects an element.
+ * @param {*} children all elements placed in between 2 dropdown tags will be send as children of this element and are placed inside the dropdown.
+ * @returns a fully functional dropdown.
+ */
 export function Dropdown(props) {
-  const state = useState();
-  const { logout } = useAuth();
+  // useState and useRef are react functies that can be used in functional components. for an explanation, google is our friend.
+  const [expanded, setExpanded] = useState('false');
+  const ref_dib = useRef(null);
 
   useEffect(() => {
-    let userSettingsButton = document.getElementById('userSettingsButton');
-    let dropdownBtn = document.getElementById('dropdownItemButton');
-    
-    userSettingsButton.addEventListener('click', (event) => {
-      alert("settings still in development");
-    })
+    const dropdownBtn = ref_dib.current;
 
+    // this function will change the attribute {aria-expanded} to true or false depending on the current value.
     dropdownBtn.addEventListener('click', (event) => {
-      dropdownBtn.setAttribute('aria-expanded', dropdownBtn.getAttribute('aria-expanded') === 'true' ? 'false' : 'true');
+      setExpanded(dropdownBtn.getAttribute('aria-expanded') === 'true' ? 'false' : 'true')
     })
   },[]);
 
   return (
+    // we use ref here bacause if we use id this component wouldn't be reusable in the same webpage because of 2 or more simular id's. but with ref we wont have this because it connects the event to the current ref holder and not all ref holders.
     <section class="dropdown">
-      <button id="dropdownItemButton" aria-expanded="false" class={`dropdown-button ${props.buttonClass}`}><i class="fa-solid fa-angle-down"></i></button>
+      <button ref={ref_dib} aria-expanded={expanded} class={`dropdown-button ${props.buttonClass}`}>{props.buttonIcon}</button>
+
       <article aria-labelledby="dropdown-item-button" class="dropdown-menu">
-        <section class="dali-user-profile">
-          <i class="fa-solid fa-circle-user"></i>
-          <h3 class="dali-user-name">Anthony Inocêncio Ramos</h3>
-          <p class="dali-user-role">Uitgever | Applicatiebeheerder</p>
-          <hr/>
-        </section>
-        <button id="userSettingsButton" class="dali-dropdown-sidebutton"><i class="fa-solid fa-gear"></i> gebruikers instellingen</button>
-        <button onClick={logout} class="dali-dropdown-sidebutton"><i class="fa-solid fa-right-from-bracket"></i> log uit</button>
+        {/* when placing more then 1 element it is recommended to wrap it in a section first. */}
+        {props.children}
       </article>
     </section>
   );
--- dali-home/src/js/components/header.js
+++ dali-home/src/js/components/header.js
@@ -1,42 +1,26 @@
-import {Navbar, Nav, Button} from 'react-bootstrap';
 import React from 'react';
-import { Dropdown } from './dropdown';
-import { ModalFooter, Popup } from './popup';
 
-
-export class Header extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {}
-  }
-
-  render(){
-    return(<>
+/**
+ * header component with preset styles.
+ * @param {*} navLeft nav items that should be on the left of the header. 
+ * @param {*} title the title of the current webpage.
+ * @param {*} navRight nav items that should be on the right of the header.
+ * @returns a prestyled header element with skip navigation link, left navigation, h1 title and right naviagtion.
+ */
+export function Header(props) {
+  return(
+    <>
       <header className='header'>
-        <a className="invisible-link header-link" href="#main">skip navigation</a>
-        <h1>DALI - toepassingen</h1>
+        {/* invisible link is a link only visible by getting focus. this is for people who don't use mouses to easily skip the navigation and go to the main */}
+        <a className="invisible-link header-link"   href="#main">skip navigation</a>
         <nav>
-          <Dropdown buttonClass="header-buttons"/>
-          <Popup 
-            showModalButtonClass="header-buttons"
-            showModalButtonContents="?"
-            modalHeaderFeatures="sm"
-            modalHeaderTitle="Help"
-            modalFeatures="gray-2-xxl"
-            modalFooterContent={
-            <ModalFooter modalFooterFeatures="xl">
-              <a>2022 - Buro CITE</a>
-              <a>Visserlaan 18 2288ER Rijswijk</a>
-              <a>Nederland</a>
-              <a target="_blank" href="https://demo.dali-app.nl/#dashboard">www.dali-app.nl</a><a>by Anthony Inocencio Ramos</a>
-            </ModalFooter>}
-          >
-            hello world!
-          </Popup>
+          {props.navLeft}
+        </nav>
+        <h1>{props.title}</h1>
+        <nav className="nav-right">
+          {props.navRight}
         </nav>
       </header>
-      
-      </>
-    );
-  }
+    </>
+  );
 } 
